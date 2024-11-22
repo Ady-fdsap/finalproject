59a5a336d3da5371c7c6ed8be9fe5ebb1e3ae74d
Ady-fdsap <youngadrielle5@gmail.com>
2024-11-22 15:36:12 +0800

    moved newgeofence func to geofencing.go

--- geofencing.go
+++ geofencing.go
@@ -26,6 +26,26 @@ type Geofence struct {
 	maxTileY    float64
 }
 
+func NewGeofence() *Geofence {
+	points := [][]*geo.Point{
+		{
+			geo.NewPoint(14.067694194798804, 121.32708640042505),
+			geo.NewPoint(14.06800445535538, 121.32742234709286),
+			geo.NewPoint(14.068129707532552, 121.32719002650704),
+			geo.NewPoint(14.06788253996273, 121.32688224303081),
+		},
+	}
+	geofence := &Geofence{}
+	geofence.tiles = make(map[float64]string)
+	geofence.granularity = 50
+	geofence.vertices = points[0]
+	if len(points) > 1 {
+		geofence.holes = points[1:]
+	}
+	geofence.setInclusionTiles()
+	return geofence
+}
+
 func handleGeofenceCheckRequest(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Received %s request from %s to %s?%s\n", r.Method, r.RemoteAddr, r.URL.Path, r.URL.Query())
 	lat := r.URL.Query().Get("lat")
--- main.go
+++ main.go
@@ -4,29 +4,8 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
-
-	geo "github.com/kellydunn/golang-geo"
 )
 
-func NewGeofence() *Geofence {
-	points := [][]*geo.Point{
-		{
-			geo.NewPoint(14.067694194798804, 121.32708640042505),
-			geo.NewPoint(14.06800445535538, 121.32742234709286),
-			geo.NewPoint(14.068129707532552, 121.32719002650704),
-			geo.NewPoint(14.06788253996273, 121.32688224303081),
-		},
-	}
-	geofence := &Geofence{}
-	geofence.tiles = make(map[float64]string)
-	geofence.granularity = 50
-	geofence.vertices = points[0]
-	if len(points) > 1 {
-		geofence.holes = points[1:]
-	}
-	geofence.setInclusionTiles()
-	return geofence
-}
 
 var db *sql.DB
 
