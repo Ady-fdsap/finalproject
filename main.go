e5828bb0c73763ff2b4b8abcbbe3bd1c665853a2
Ady-fdsap <youngadrielle5@gmail.com>
2024-11-25 15:48:22 +0800

    optimized some

--- main.go
+++ main.go
@@ -6,16 +6,15 @@ import (
 	"net/http"
 )
 
-
 var db *sql.DB
 
 func main() {
-	log.Println("API up and running :) ")
 	initDB()
 	api := &API{database: db}
 	go menu()
 	http.HandleFunc("/geofence/check", corsMiddleware(Logger(handleGeofenceCheckRequest)))
 	http.HandleFunc("/employee/login", corsMiddleware(api.handleEmployeeLogin))
+	log.Println("API up and running :) ")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
