a6ea8f9de153c782d94f102162da3ffb3b3a939f
Ady-fdsap <youngadrielle5@gmail.com>
2024-11-26 08:24:59 +0800

    fixed code flow

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ var db *sql.DB
 func main() {
 	initDB()
 	api := &API{database: db}
-	go menu()
 	http.HandleFunc("/geofence/check", corsMiddleware(Logger(handleGeofenceCheckRequest)))
 	http.HandleFunc("/employee/login", corsMiddleware(api.handleEmployeeLogin))
 	log.Println("API up and running :) ")
+	go menu()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
